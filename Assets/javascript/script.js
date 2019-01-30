17dde39d68fdf79d9e7fe5b3e3efa57ab4a7c2b3
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-01-29 19:09:07 -0800

    done 1/29/19

---
+++ Assets/javascript/script.js
@@ -0,0 +1,67 @@
+var config = {
+    apiKey: "AIzaSyB4NSYuISvalQInheKFysQ0vuU7pFh2CXg",
+    authDomain: "train-schedule-19d5b.firebaseapp.com",
+    databaseURL: "https://train-schedule-19d5b.firebaseio.com",
+    projectId: "train-schedule-19d5b",
+    storageBucket: "train-schedule-19d5b.appspot.com",
+    messagingSenderId: "66370288315"
+};
+firebase.initializeApp(config);
+
+var trainDatabase = firebase.database();
+
+// show current time
+function time() {
+    now = moment().format("HH:mm");
+    time = $("<p class='text-center'>").html("<strong>Current time: " + now + "</strong>");
+    $("#clock").append(time);
+}
+time();
+
+// add input values to database when click submit
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    // initialize new train object
+    var newTrain = {
+        name: "",
+        destination: "",
+        firstTrain: "",
+        frequency: 0,
+    };
+
+    // push input values into object
+    newTrain.name = $("#train-name").val().trim();
+    newTrain.destination = $("#destination").val().trim();
+    newTrain.firstTrain = $("#first-train-time").val();
+    newTrain.frequency = $("#frequency").val();
+
+    console.log(newTrain);
+    // push object into database
+    trainDatabase.ref().push(newTrain);
+
+    // clear input forms
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+})
+
+trainDatabase.ref().on("child_added", function(childSnapshot) {
+
+    // time calculations
+    var tFreq = childSnapshot.val().frequency;
+    var tFirstTrain = childSnapshot.val().firstTrain;
+    var tFirstConverted = moment(tFirstTrain, "HH:mm").subtract(1, "years");
+    var tDiff = moment().diff(moment(tFirstConverted), "minutes");
+    var tRemainder = tDiff % tFreq;
+    var minAway = tFreq - tRemainder;
+    var nextArrival = moment().add(minAway, "minutes");
+
+    // add everything to the table
+    $("#table-body").prepend("<tr><th scope='row'>" + childSnapshot.val().name + "</th><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().frequency + "</td><td>" + nextArrival.format("HH:mm") + "</td><td>" + minAway + "</td></tr>");
+
+    // Handle the errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+  });
